3e05dde8417974526316db06129bb9b0188700d0
Manish Patil <ManishPatil159@users.noreply.github.com>
2017-04-17 19:40:37 +0530

    Initial Commit
    Initial commit.
    1. Entity frameword added (Code first)
    2. JWT authentication added

---
+++ Models/User.cs
@@ -0,0 +1,12 @@
+﻿
+
+namespace EntityAPI.Models
+{
+    public class User
+    {
+        public int ID { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public int StudentId { get; set; }
+    }
+}
---
+++ Startup.Auth.cs
@@ -0,0 +1,75 @@
+﻿using EntityAPI.Data;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Security.Claims;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using EntityAPI.Models;
+using System.Linq;
+using EntityAPI.Encryption;
+
+namespace EntityAPI
+{
+    public partial class Startup
+    {
+        private SchoolContext _sCcontext;
+        private void ConfigureAuth(IApplicationBuilder app)
+        {
+
+            var signingKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(Configuration.GetSection("TokenAuthentication:SecretKey").Value));
+
+            var tokenProviderOptions = new TokenProviderOptions
+            {
+                Path = Configuration.GetSection("TokenAuthentication:TokenPath").Value,
+                Audience = Configuration.GetSection("TokenAuthentication:Audience").Value,
+                Issuer = Configuration.GetSection("TokenAuthentication:Issuer").Value,
+                SigningCredentials = new SigningCredentials(signingKey, SecurityAlgorithms.HmacSha256),
+                IdentityResolver = GetIdentity
+            };
+
+            var tokenValidationParameters = new TokenValidationParameters
+            {
+                // The signing key must match!
+                ValidateIssuerSigningKey = true,
+                IssuerSigningKey = signingKey,
+                // Validate the JWT Issuer (iss) claim
+                ValidateIssuer = true,
+                ValidIssuer = Configuration.GetSection("TokenAuthentication:Issuer").Value,
+                // Validate the JWT Audience (aud) claim
+                ValidateAudience = true,
+                ValidAudience = Configuration.GetSection("TokenAuthentication:Audience").Value,
+                // Validate the token expiry
+                ValidateLifetime = true,
+                // If you want to allow a certain amount of clock drift, set that here:
+                ClockSkew = TimeSpan.Zero
+            };
+
+            app.UseJwtBearerAuthentication(new JwtBearerOptions
+            {
+                AutomaticAuthenticate = true,
+                AutomaticChallenge = true,
+                TokenValidationParameters = tokenValidationParameters
+            });
+
+            _sCcontext = app.ApplicationServices.GetService<SchoolContext>();
+            app.UseMiddleware<TokenProviderMiddleware>(Options.Create(tokenProviderOptions));
+        }
+
+        private Task<ClaimsIdentity> GetIdentity(string username, string password)
+        {
+            User userCredentials = _sCcontext.Users.FirstOrDefault(u => u.Username.Equals(username) && u.Password.Equals(Security.Hash(password)));
+            if (userCredentials != null)
+            {
+                return Task.FromResult(new ClaimsIdentity(new GenericIdentity(username, "Token"), new Claim[] { }));
+            }
+
+            // Account doesn't exists
+            return Task.FromResult<ClaimsIdentity>(null);
+        }
+
+    }
+}
